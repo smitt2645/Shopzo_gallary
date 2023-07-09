b9ba777ecc21f02a903d827d698263ad7aa7bab7
smitt2645 <124039635+smitt2645@users.noreply.github.com>
2023-07-09 11:49:23 +0530

    Update gallary.js

--- gallary.js
+++ gallary.js
@@ -1,1 +1,14 @@
 
+var card = document.querySelectorAll('.card');
+
+function show(n){
+ var chld = card[n].children[2];
+ chld.style.display = 'block';
+
+}
+
+function hide(n){
+ var chld = card[n].children[2];
+ chld.style.display = 'none';
+ 
+}
